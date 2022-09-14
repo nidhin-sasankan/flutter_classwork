0d56ace9f69f3c67ddc1e5d66523379ba3e6770b
The Coding Hive <nidhinsasankan@gmail.com>
2022-09-14 18:08:14 +0530

    registration

---
+++ flutter_assignments/lib/registration.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'Homepage.dart';
+
+void main() {
+  runApp(Registration());
+}
+
+class Registration extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Register"),
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Text(
+                  "Registration",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                    decoration: InputDecoration(
+                        label: Text("Name"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        hintText: "Enter Username")),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                    decoration: InputDecoration(
+                        label: Text("Email"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        hintText: "Enter Email")),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                        label: Text("Password"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        hintText: "Enter Password")),
+              ),
+              Padding(padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Homepage()));
+                }
+              ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
