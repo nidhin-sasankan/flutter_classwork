36e685eff24fb2379d7ec3a43041d2506f586886
The Coding Hive <nidhinsasankan@gmail.com>
2022-09-21 11:09:45 +0530

    welcome page updated

--- flutter_assignments/lib/welcome.dart
+++ flutter_assignments/lib/welcome.dart
@@ -3,52 +3,60 @@ import 'package:flutter_assignments/register.dart';
 
 import 'login.dart';
 
-void main(){
+void main() {
   runApp(Dashboard());
 }
 
-class Dashboard extends StatelessWidget{
+class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-       body: Center(
-         child: Column(
-           children: [
-             Padding(
-               padding: const EdgeInsets.fromLTRB(30, 70, 30, 30),
-               child: Text("Welcome", style: TextStyle(fontSize: 28),),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(30.0),
-               child: Image(
-                 image: AssetImage("assets/images/welcome.jpg"),
-               )
-             ),
-             Padding(
-               padding: const EdgeInsets.fromLTRB(30, 150, 30, 0),
-               child: ElevatedButton(
-                 style: ElevatedButton.styleFrom(
-                     minimumSize: Size.fromHeight(40)),
-                 onPressed: () {},
-                 child: Text("Login"),
-               ),
-             ),
-             Padding(
-               padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
-               child: ElevatedButton(
-                 style: ElevatedButton.styleFrom(
-                     minimumSize: Size.fromHeight(40)),
-                 onPressed: () {},
-                 child: Text("Sign up"),
-               ),
-             ),
-           ],
-         ),
-       ),
-     ),
-   );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 70, 30, 30),
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Image(
+                    image: AssetImage("assets/images/welcome.jpg"),
+                  )),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 100, 30, 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size.fromHeight(40)),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginChk()));
+                  },
+                  child: Text("Login"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size.fromHeight(40)),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                        Register()));
+                  },
+                  child: Text("Sign up"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  
 }
