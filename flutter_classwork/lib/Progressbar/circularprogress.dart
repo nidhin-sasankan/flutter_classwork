a0f47972e4d1ffd718f1cfc8f1cbcb744b738212
The Coding Hive <nidhinsasankan@gmail.com>
2022-10-13 16:08:55 +0530

    indeterminate and determinate progressbar

--- flutter_classwork/lib/Progressbar/circularprogress.dart
+++ flutter_classwork/lib/Progressbar/circularprogress.dart
@@ -1,34 +1,95 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: CircleProg(),
   ));
 }
 
-class CircleProg extends StatelessWidget {
+class CircleProg extends StatefulWidget {
+  @override
+  State<CircleProg> createState() => _CicleprogState();
+}
+
+class _CicleprogState extends State<CircleProg> {
+  double value  = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Circular Progressbar"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Indeterminate Circular Progressbar"),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Center(
-              child: CircularProgressIndicator(
-                backgroundColor: Colors.grey,
-                strokeWidth: 5,
-              ),
-            ),
-          )
-        ],
-      ),
+        appBar: AppBar(
+          title: Text("Flutter Circular Progress Indicator"),
+        ),
+        body: Container(
+            alignment: Alignment.topCenter,
+            margin: EdgeInsets.only(top: 20),
+            child: Column(
+                children:[
+                  Container(
+                    child: Text("Indeterminate Progress Indicator",style: TextStyle(fontSize: 18),),
+                    margin: EdgeInsets.all(20),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.grey,
+                      color: Colors.purple,
+                      strokeWidth: 5,
+                    ),
+                  ),
+                  Container(
+                    child: Text("Determinate Progress Indicator",style: TextStyle(fontSize: 18)),
+                    margin: EdgeInsets.all(20),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.grey,
+                      color: Colors.green,
+                      strokeWidth: 5,
+                      value: value,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      child: Text("Download File"),
+                      style: ElevatedButton.styleFrom(
+                          onPrimary: Colors.white,
+                          primary: Colors.green
+                      ),
+                      onPressed: ()
+                      {
+                        value = 0;
+                        downloadData();
+                        setState(() {
+
+                        });
+                      },
+                    ),
+                  )
+                ]
+            )
+        )
+    );
+  }
+
+  void downloadData(){
+    new Timer.periodic(
+        Duration(seconds: 1),
+            (Timer timer){
+          setState(() {
+            if(value == 1) {
+              timer.cancel();
+            }
+            else {
+              value = value + 0.1;
+            }
+          });
+        }
     );
   }
 }
--- flutter_classwork/lib/Progressbar/horzontalprogress.dart
+++ flutter_classwork/lib/Progressbar/horzontalprogress.dart
@@ -1,11 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: Horizonprog(),));
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Horizonprog(),
+  ));
 }
 
-class Horizonprog extends StatelessWidget{
+class Horizonprog extends StatefulWidget {
+  @override
+  State<Horizonprog> createState() => _HorizonprogState();
+}
+
+class _HorizonprogState extends State<Horizonprog> {
+  double value = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,18 +27,64 @@ class Horizonprog extends StatelessWidget{
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Indeterminate Progressbar"),
-          Padding(
+          Container(
+            child: Text(
+              "Indeterminate Progress Indicator",
+              style: TextStyle(fontSize: 18),
+            ),
+            margin: EdgeInsets.all(20),
+          ),
+          Container(
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: LinearProgressIndicator(
                 minHeight: 5,
-                backgroundColor: Colors.black12,
+                backgroundColor: Colors.grey,
+                color: Colors.purple,
               ),
+            ),
+          ),
+          Container(
+            child: Text("Determinate Progress Indicator",
+                style: TextStyle(fontSize: 18)),
+            margin: EdgeInsets.all(20),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.grey,
+              color: Colors.green,
+              minHeight: 5,
+              value: value,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: ElevatedButton(
+              child: Text("Download File"),
+              style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.white, primary: Colors.green),
+              onPressed: () {
+                value = 0;
+                downloadData();
+                setState(() {});
+              },
             ),
           )
         ],
       ),
     );
+  }
+
+  void downloadData() {
+    new Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      setState(() {
+        if (value == 1) {
+          timer.cancel();
+        } else {
+          value = value + 0.1;
+        }
+      });
+    });
   }
 }
